e80aa2e3677e60723b6bed8c336209231ea0b17b
Orare <1163182+orare@users.noreply.github.com>
2023-08-17 13:24:04 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Tracking Twitch.tv/Quin69's Chat-Requested Songs
+
+This project tracks songs requested in Twitch.tv/Quin69's chat! It builds a playlist of viewer-requested songs using Sheepfarmer's chat logs.
+
+**How It Works:**
+
+We use Sheepfarmer's chat logs to record song requests, keeping the playlist up-to-date. Note that the accuracy depends on whether Quin69 uses the chat's playlist.
+
+**Keeping It Accurate:**
+
+If you spot a wrong song, it could be because:
+
+- Quin69 isn't using the chat's playlist at that moment.
+- The queue is out of date. To fix this, type `!song` in Quin69's chat to update the current song.
---
+++ src/script.js
@@ -0,0 +1,79 @@
+async function updatePlaylist() {
+    try {
+        const response = await fetch("https://logs.ivr.fi/channel/quin69/user/sheepfarmer/?reverse");
+        const text = await response.text();
+
+        const lines = text.split("\n");
+
+        const { currentSongTitle, queueTitles } = parsePlaylist(lines);
+
+        displayCurrentSong(currentSongTitle);
+        displayQueue(queueTitles, currentSongTitle);
+    } catch (error) {
+        console.error("An error occurred while fetching or processing the playlist:", error);
+    }
+}
+
+function parsePlaylist(lines) {
+    let currentSongTitle = null;
+    let queueTitles = [];
+
+    for (const line of lines) {
+        if (line.includes("🔊") && !line.includes("VIBE")) {
+            currentSongTitle = line.substring(line.indexOf("🔊") + 2).trim();
+            break;
+        }
+    }
+
+    queueTitles = lines
+        .filter(line => line.includes("➕"))
+        .map(line => line.substring(line.indexOf("➕") + 2).trim());
+
+    return { currentSongTitle, queueTitles };
+}
+
+function displayCurrentSong(song) {
+    const currentSongElement = document.getElementById("currentSong");
+    currentSongElement.innerHTML = ""; // Clear existing content
+
+    if (song) {
+        const link = document.createElement("a");
+        link.textContent = song;
+        link.href = `https://www.youtube.com/results?search_query=${encodeURIComponent(song)}`;
+        link.target = "_blank";
+
+        currentSongElement.appendChild(link);
+    } else {
+        currentSongElement.textContent = "No song is currently playing.";
+    }
+}
+
+
+function displayQueue(queue, currentSong) {
+    const queueElement = document.getElementById("queue");
+    queueElement.innerHTML = ""; 
+
+    for (const song of queue) {
+        const li = document.createElement("li");
+        const link = document.createElement("a");
+
+        if (song === currentSong) {
+			break;
+        } else {
+            link.textContent = song;
+            link.href = `https://www.youtube.com/results?search_query=${encodeURIComponent(song)}`;
+            link.target = "_blank";
+        }
+
+        li.appendChild(link);
+        queueElement.appendChild(li);
+    }
+
+    if (queueElement.childElementCount === 0) {
+        const emptyMessage = document.createElement("li");
+        emptyMessage.textContent = "The queue is empty.";
+        queueElement.appendChild(emptyMessage);
+    }
+}
+
+window.onload = updatePlaylist;
